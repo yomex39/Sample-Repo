3e0d43f69beed7769efc8695e98d471d5e53c368
Philip Ilori <yomex39@yahoo.com>
2020-06-08 15:24:56 -0400

    add the questions to the file

--- Network_Training/lesson1/exercise1.py
+++ Network_Training/lesson1/exercise1.py
@@ -1,3 +1,11 @@
+"""  Create a Python script that has three variables: ip_addr1, ip_addr2, ip_addr3 (representing three corresponding IP addresses). Print these three variables to standard output using a single print statement.
+
+Make your print statement compatible with both Python2 and Python3.
+
+If you are using either Linux or MacOS make your program executable by adding a shebang line and by changing the files permissions (i.e. chmod 755 exercise1.py).
+"""
+# Solution
+
 from __future__ import print_function
 
 ip_addr1 = "192.168.1.10"
--- Network_Training/lesson1/exercise2.py
+++ Network_Training/lesson1/exercise2.py
@@ -1,3 +1,22 @@
+"""Prompt a user to enter in an IP address from standard input. Read the IP address in and break it up into its octets. Print out the octets in decimal, binary, and hex.
+
+Your program output should look like the following:
+
+​ $ python exercise2.py 
+Please enter an IP address: 80.98.100.240
+
+    Octet1         Octet2         Octet3         Octet4     
+------------------------------------------------------------
+      80             98             100            240      
+   0b1010000      0b1100010      0b1100100     0b11110000   
+     0x50           0x62           0x64           0xf0      
+------------------------------------------------------------
+
+Four columns, fifteen characters wide, a header column, data centered in the column.
+"""
+
+# Solution
+
 from __future__ import print_function
 
 try:
