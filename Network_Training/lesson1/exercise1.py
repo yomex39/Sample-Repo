7529d528341341819116da1e657dc96a15a05b34
Philip Ilori <yomex39@yahoo.com>
2020-06-08 14:45:45 -0400

    exercise 1

---
+++ Network_Training/lesson1/exercise1.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+ip_addr1 = "192.168.1.10"
+ip_addr2 = "192.168.2.10"
+ip_addr3 = "192.168.3.10"
+# print these ip addresses to terminal
+print(ip_addr1, ip_addr2, ip_addr3)
