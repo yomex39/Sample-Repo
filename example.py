e408ef492bfab749a221bac8709b66ad9dcb4b31
Philip Ilori <yomex39@yahoo.com>
2020-06-07 22:18:48 -0400

    made a new file

---
+++ example.py
@@ -0,0 +1,7 @@
+character_name = "Peter"
+character_age = "30"
+
+print("There is a man named " + character_name + ",")
+print("He is " + character_age + " years old.")
+print("He like being married and being " + character_name + ",")
+print("But he doesn't like being " + character_age + ".")
